ccb04f8531f889fd967a1bda36bf6c086b1f6549
mathteacher123 <115059290+mathteacher123@users.noreply.github.com>
2025-08-29 06:52:06 +0000

    feat(demo): connect with wp-mcp server

--- test/agent.js
+++ test/agent.js
@@ -11,7 +11,20 @@ import { tool } from "@langchain/core/tools";
 //require('dotenv').config();
 import 'dotenv/config';
 
+import { loadMcpTools, MultiServerMCPClient } from "@langchain/mcp-adapters";
+//console.log('token', process.env.WP_MCP_TOKEN)
+const client = new MultiServerMCPClient({
+   "wordpress-mcp": {
+        transport:'http',
+        "url": "http://wps.zya.me/wp-json/wp/v2/wpmcp/streamable",
+        headers: {
+            "Authorization": `Bearer ${process.env.WP_MCP_TOKEN}`
+        }
+    }
+});
 
+const wpTools = await client.getTools();
+//console.log(wpTools)
 const getWeather = tool(async (input) => {
 const input1 = input;
   if (input1.city === "nyc") {
@@ -38,7 +51,8 @@ async function main(){
     });
 
     // Initialize memory to persist state between graph runs
-    //const agentCheckpointer = new MemorySaver();
+    const agentCheckpointer = new MemorySaver();
+/*    
     let constr = 'postgresql://postgres:Aaaaaa@0@db.jbksykhhmfmzkpzydpgm.supabase.co:5432/postgres';
     //const agentCheckpointer = PostgresSaver.fromConnString(constr);
     constr = process.env.DATABASE_URL;
@@ -51,12 +65,37 @@ async function main(){
     const agentCheckpointer = new PostgresSaver(pool);
     //const aa = await agentCheckpointer.setup();
     //console.log('scheam crated', aa)
+*/    
     const agent = createReactAgent({
     llm: agentModel,
-    tools: agentTools,
+    tools: wpTools,//agentTools,
     checkpointSaver: agentCheckpointer,
     });
     const config = { configurable: { thread_id: "7" } };
+
+    let inputs = { messages: [{ role: "user",  content: "add a new page. write a 3d game idea and reason why this game will be fun and exciting to play. suggest me a web based 3d game engine for coding. in the end, add your signature so reader will know that this post was written by llm. after adding this page, give me its name, slug and link." }] };
+
+
+    for await (
+        const chunk of await agent.stream(inputs, {
+            ...config,
+            streamMode: "updates",
+        })
+    ) {
+        for (const [node, values] of Object.entries(chunk)) {
+            console.log(`Receiving update from node: ${node}`);
+            var msg = values.messages[0];
+            if (msg.tool_calls?.length){
+                console.log(msg.tool_calls)
+            }
+            else if (node === 'agent'){
+                console.log(msg.content, msg.constructor.name)
+            }
+            console.log("\n====\n");
+        }
+    }
+
+/*
     // Now it's time to use!
     const agentFinalState = await agent.invoke(
     { messages: [new HumanMessage("what is the current weather in sf")] },
@@ -76,6 +115,6 @@ async function main(){
     agentNextState.messages[agentNextState.messages.length - 1].content,
     );
     console.log('checkpointer state', await agentCheckpointer.get(config));
-
+*/
 }
 main();
