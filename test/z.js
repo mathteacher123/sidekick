c5b349f05208c81dc8755a407057e23e75c1d2ad
mathteacher123 <115059290+mathteacher123@users.noreply.github.com>
2025-09-04 10:09:18 +0000

    refactor(demo) jsonoutputparser sets the tool_call type to the name of tool

--- test/z.js
+++ test/z.js
@@ -1,5 +1,35 @@
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
+import { JsonOutputToolsParser } from "@langchain/core/output_parsers/openai_tools";
+import { tool } from "@langchain/core/tools";
+import { createModel, loadJSONFile } from "./utils.js";
+import "dotenv/config";
+const responseObject = z.object({
+  iuy: z.string().describe("Response to user."),
+});
+const responseTool = tool(() => {
+  console.log('tool called')
+}, {
+  name: "response123",
+  description: "Respond to the user.",
+  schema: responseObject,
+});
+const log=(x)=>{
+  console.dir('log')
+  console.dir(x, {depth:null})
+  return  x;
+}
+const parser = new JsonOutputToolsParser();
+const replanner =
+    createModel().bindTools([
+      responseTool,
+    ])
+  .pipe(log)
+  .pipe(parser);
+let s=await replanner.invoke(['user',"reply to user, 'byyyy'"]);
+console.dir(s, {depth:null})
+process.exit(0)
+
 
 const mySchema = z
   .object({
